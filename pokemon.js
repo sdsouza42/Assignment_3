7d4350a1f55b68dadd8a2a7f1e5d0875c822feab
sdsouza42 <dsouza.savio@hotmail.com>
2023-05-15 00:14:54 -0700

    Created pagination to display 5 pokemons at time

--- pokemon.js
+++ pokemon.js
@@ -1,16 +1,25 @@
 // URL for the Pokémon API
-const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=810';
+const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=5';
+
+// Variables to keep track of the current page and the total number of pages
+let currentPage = 1;
+let totalPages = 0;
 
 // Function to fetch Pokémon data from the API
-function fetchPokemon() {
-    axios.get(apiUrl)
-        .then(response => displayPokemon(response.data.results))
+function fetchPokemon(page) {
+    axios.get(`${apiUrl}&offset=${(page - 1) * 5}`)
+        .then(response => {
+            totalPages = Math.ceil(response.data.count / 5);
+            displayPokemon(response.data.results);
+            displayPagination();
+        })
         .catch(error => console.error(error));
 }
 
 // Function to display Pokémon on the page
 function displayPokemon(pokemonList) {
     const pokemonContainer = document.getElementById('pokemon-list');
+    pokemonContainer.innerHTML = ''; // Clear existing Pokémon
 
     pokemonList.forEach(pokemon => {
         const pokemonElement = document.createElement('div');
@@ -29,5 +38,34 @@ function displayPokemon(pokemonList) {
     });
 }
 
+// Function to display pagination buttons
+function displayPagination() {
+    const pagination = document.getElementById('pagination');
+    pagination.innerHTML = '';  // Clear existing pagination buttons
+
+    let startPage = Math.max(currentPage - 2, 1);
+    let endPage = Math.min(startPage + 4, totalPages);
+    startPage = Math.max(endPage - 4, 1);
+
+    for (let i = startPage; i <= endPage; i++) {
+        const listItem = document.createElement('li');
+        listItem.classList.add('page-item');
+        if (i === currentPage) {
+            listItem.classList.add('active');
+        }
+
+        const link = document.createElement('a');
+        link.classList.add('page-link');
+        link.textContent = i;
+        link.addEventListener('click', () => {
+            currentPage = i;
+            fetchPokemon(currentPage);
+        });
+
+        listItem.appendChild(link);
+        pagination.appendChild(listItem);
+    }
+}
+
 // Fetch and display Pokémon when the page loads
-fetchPokemon();
+fetchPokemon(currentPage);
